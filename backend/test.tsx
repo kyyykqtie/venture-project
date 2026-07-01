0a79df1534cc720034dbc7defe8239b01e2ba08b
kyyykqtie <marknisnea06@gmail.com>
2026-07-02 00:06:55 +0800

    first commit

---
+++ backend/test.tsx
@@ -0,0 +1,9 @@
+
+
+function test(){
+    return (
+        <h1>Hello World </h1>
+    )
+}
+
+export default test;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold underline text-blue-200  items-center text-center px-4">
+      Hello
+    </h1>
+  )
+}
+
+export default App
